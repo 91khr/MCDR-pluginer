0c18044fdcfac025e99c3ac3a0427c9a5432dbba
Virginia Senioria <91khr@users.noreply.github.com>
2020-07-15 13:03:43 +0800

    Added dependencies and subconfig support.
    Not tested.

--- README.md
+++ README.md
@@ -2,24 +2,28 @@
 
 A plugin manager for MCDR
 
-## Requirements
+# Requirements
 
 Almost everything MCDR-pluginer requires is also required by MCDR.
 By default, MCDR-pluginer uses git to manage plugins,
 but this can be changed by replacing the `handle.py`.
 
-## Usage
+# Installation
 
-Just run `./pluginer.py`, and MCDR-pluginer will synchronize all,
-this action includes installing and updating all plugins configurated,
-and remove all plugins that are not configurated.
+Just clone the repo, and it's ready for use.
+
+# Usage
+
+Just run `./pluginer.py`, and MCDR-pluginer will synchronize everything,
+including installing and updating all plugins configurated,
+and removing all non-configurated plugins.
 
 **WARNING**: MCDR-pluginer would remove all unrecognized symlinks in the plugins directory.
 
-## Configuration
+# Configuration(for server operators)
 
 All configurations are in the `config.yaml`, it's with comment defaultly.
-Since the file is added to the ignore list, changing it wouldn't affect remote repository.
+The file is ignored by default, therefore changing it wouldn't affect remote repository.
 
 In fact, our default configuration assume that
 you have such a directory structure in your server:
@@ -34,30 +38,37 @@ server/
 | plugin_repo/
 ```
 
-### `plugins_dir`
+## `plugins_dir`
 
 The directory for MCDR to read plugins.
 
 type: string
 
-### `repo_dir`
+## `repo_dir`
 
 The directory to store plugin repository
 
 type: string
 
-### `plugins`
+## `plugins`
 
 The plugin list.
-
-type: list
-
 Each of the item of the list describes a plugin,
 which can be either a string or an object.
 In the case when it's a string, it stands for the `url` field of the object.
 Unnecessary fields of the object would be generated from the necessary fields.
 
-#### `url`
+type: list
+
+example:
+```yaml
+plugins:
+  - TISUnion/Here
+  - url: 91khr/QuickBackupM
+    content: QuickBackupM.py
+```
+
+### `url`
 
 The URL of the plugin repository.
 When the repository is hosted in github, it needn't have to be complete.
@@ -68,7 +79,7 @@ type: string
 
 example: `https://github.com/TISUnion/Here` can be abbreviated to `TISUnion/Here`.
 
-#### `content`
+### `content`
 
 The filename of the plugin file.
 Defaultly the same as the plugin name.
@@ -78,4 +89,30 @@ Can also be a list containing all essential files (and folders) of the plugin --
 that's why the field is not named `filename`.
 
 type: string | list[string]
+
+### `dependencies`
+
+The dependencies of the plugin.
+Usually this is done by the plugin author
+(as long as there is a `plugin_info.yaml` in the plugin directory),
+but in case when this isn't done, you should declare this manually.
+
+type: list, each of the items has the same type as items in [`plugins`](#plugins)
+
+# Configuration(for plugin developers)
+
+Some configurations may be made in plugin repository,
+thus the installation of the plugin for MCDR-pluginer users can be simplified.
+
+The keys are the same as those of any item in the key `plugins` of the main configuration.
+
+## `content`
+
+The filename of the plugin file, or a list of plugin files.
+Would be overrided by the main configuration
+
+## `dependencies`
+
+The dependencies of the plugin.
+The dependencies configurated in the main configuration would be used to update this list.
 
--- pluginer.py
+++ pluginer.py
@@ -11,58 +11,73 @@ from status import PluginState, PluginAction
 
 class Pluginer:
     # {{{ Init
-    def _load_conf(self):
+    def _load_conf(self, default, location = "config.yaml"):
         """ Load the pluginer configuration """
         # Read config file
         try:
-            with open("config.yaml", "r") as f:
+            with open(location, "r") as f:
                 self.conf = YAML(typ="safe").load(f)
         except FileNotFoundError:
             self.conf = {}
 
         # Set default values for the key
-        def touch_key(key, default):
+        def touch_key(key):
             if key not in self.conf:
-                self.conf[key] = default
-        touch_key('plugins_dir', 'MCDR/plugins')
-        touch_key('repo_dir', 'plugin_repo')
-        touch_key('plugins', [])
+                self.conf[key] = default[key]
+        touch_key('plugins_dir')
+        touch_key('repo_dir')
+        touch_key('plugins')
 
 
 
-    def _init_plg(self):
+    def _init_plg(self, plg_key = 'plugins'):
         """ Init the plugins """
         self.plgs = {}
-        for nowplg in self.conf['plugins']:
-            if isinstance(nowplg, dict):
-                it = { 'url': nowplg['url'] }
-                if 'content' in nowplg:
-                    it['content'] = nowplg['content']
-            elif isinstance(nowplg, str):
-                it = { 'url': nowplg }
-            else:
-                raise TypeError("The type of a plugin item can only be str or dict")
+        plg_list = self.conf[plg_key]
+        plg_next = []
+        while plg_list:
+            for nowplg in plg_list:
+                if isinstance(nowplg, dict):
+                    it = { 'url': nowplg['url'] }
+                    if 'content' in nowplg:
+                        it['content'] = nowplg['content']
+                    if 'dependencies' in nowplg:
+                        deps = nowplg['dependencies']
+                        if not isinstance(deps, list):
+                            raise TypeError("Dependencies must be a list!")
+                        plg_next += deps
+                elif isinstance(nowplg, str):
+                    it = { 'url': nowplg }
+                else:
+                    raise TypeError("The type of a plugin item can only be str or dict")
 
-            # Process the url
-            if not (it['url'].startswith('http://')
-                    or it['url'].startswith('https://')) and ('/' in it['url']):
-                it['url'] = "https://github.com/" + it['url']
+                # Process the url
+                if not (it['url'].startswith('http://')
+                        or it['url'].startswith('https://')) and ('/' in it['url']):
+                    it['url'] = "https://github.com/" + it['url']
 
-            name = it['url'][it['url'].rfind('/') + 1:]
-            if name in self.plgs:
-                raise ValueError("There cannot be two plugins with the same name!")
-            self.plgs[name] = it
-    # }}} End init
+                name = it['url'][it['url'].rfind('/') + 1:]
+                if name in self.plgs:
+                    raise ValueError("There cannot be two plugins with the same name!")
+                self.plgs[name] = it
+            plg_list = plg_next
+            plg_next = []
 
 
 
     def init(self) -> None:
         """ Init the pluginer """
-        self._load_conf()
+        self._load_conf({
+            'plugins_dir': '../MCDR/plugins',
+            'repo_dir': '../plugin_repo',
+            'plugins': [],
+            })
         self._init_plg()
+    # }}} End init
 
 
 
+    # {{{ Query
     def query(self) -> typing.Mapping[str, PluginState]:
         """ Query the state of all plugins """
         res = {}
@@ -73,6 +88,7 @@ class Pluginer:
             else:
                 res[name] = handle.query(repopath)
         return res
+    # }}} End query
 
 
 
@@ -82,19 +98,21 @@ class Pluginer:
             repopath = path.join(self.conf['repo_dir'], name)
             def linkfile(name):
                 dstfile = path.join(self.conf['plugins_dir'], name)
-                os.remove(dstfile)
+                if path.exists(dstfile): os.remove(dstfile)
                 os.symlink(path.join(repopath, name), dstfile)
             nowplg = self.plgs[name]
             if 'content' not in nowplg:
                 # Gauss one: is it the same as name
                 if path.exists(path.join(repopath, f"{name}.py")):
                     linkfile(f"{name}.py")
+                    nowplg['content'] = f"{name}.py"
                     continue
                 # That's it if there's only one file...
                 filelist = list(filter(lambda f: path.splitext(f.name),
                     os.scandir(repopath)))
                 if len(filelist) == 1:
                     linkfile(filelist[0])
+                    nowplg['content'] = filelist[0]
                 else:
                     print(f"Dont know what's the plugin, alternatives: {filelist}")
                 # Normal cases: content is given
@@ -107,23 +125,32 @@ class Pluginer:
     def sync(self) -> typing.Mapping[PluginState, typing.List[str]]:
         """ Sync all plugins, return all non operated plugins """
         state = self.query()
-        non_operated = {}
+        sub_state = {}
 
         for name in state:
             # Perform operation
             if state[name] in [PluginState.Diverged, PluginState.UpToDate]:
-                non_operated[name] = state[name]
                 continue
             operation = {
                     PluginState.NotInstalled: PluginAction.Install,
                     PluginState.NeedUpdate: PluginAction.Update,
                     }
-            handle.sync(path.join(self.conf['repo_dir'], name),
-                    operation[state[name]], self.plgs[name]['url'])
+            repopath = path.join(self.conf['repo_dir'], name)
+            handle.sync(repopath, operation[state[name]], self.plgs[name]['url'])
+
+            # Scan plugin repository for configuration
+            sub = Pluginer()
+            sub._load_conf(self.conf, repopath)
+            sub._init_plg('dependencies')
+            self.plgs.update(sub.plgs)
+            sub_state += sub.sync()
+            if 'content' in sub.conf and 'content' not in self.plgs[name]:
+                self.plgs[name]['content'] = sub.conf['content']
 
         self._link_files(state)
 
-        return non_operated
+        state.update(sub_state)
+        return state
     # }}} End synchronize
 
 
@@ -142,18 +169,18 @@ class Pluginer:
 
 
 
-def main():
-    plg = Pluginer()
-    plg.init()
-    plg.clean()
-    state = plg.sync()
-    for [nowstat, prompt] in [
-            (PluginState.UpToDate, "up to date"),
-            (PluginState.Diverged, "diverged with upstream")]:
-        print("these plugins are {}, so they're not synchronized: {}".format(prompt,
-            list(filter(lambda name: state[name] == nowstat, state))))
-
 if __name__ == "__main__":
+    def main():
+        plg = Pluginer()
+        plg.init()
+        plg.clean()
+        state = plg.sync()
+        for [nowstat, prompt] in [
+                (PluginState.UpToDate, "up to date"),
+                (PluginState.Diverged, "diverged with upstream")]:
+            print("these plugins are {}, so they're not synchronized: {}".format(prompt,
+                list(filter(lambda name: state[name] == nowstat, state))))
+
     main()
 
 # vim: fdm=marker
